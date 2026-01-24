989b68d061798e5847f14929fa0289bfe49753ae
Newton Katete <newtonisaackatete@gmail.com>
2026-01-24 12:00:29 +0200

    Basic knowledge understanding of how components work in React Native by including the Image, Text Componenets also undertansidng how the View Componenet works and also how the different dtyles work in stylesheet.create at the bottom.

---
+++ app/index.jsx
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+import Logo from '../assets/img/logo_light.png'
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+    <Image source={Logo} style={styles.img} />
+
+      <Text style={styles.title}>The Number 1</Text>
+
+      <Text style={{marginTop: 10, marginBottom: 30}}>
+        Reading list App
+      </Text>
+
+      <View style={styles.card}>
+        <Text>Hello, this is a card.</Text>
+      </View>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    img: {
+        marginVertical: 20,
+    },
+    card: {
+        background: 'green',
+        padding: 20,
+        borderRadius: 5,
+        boxShadow: '4px 4px rgba(0, 0, 0, 0.1)'
+    }
+})
